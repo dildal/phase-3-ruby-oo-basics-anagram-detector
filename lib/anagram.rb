5a981e7f8ebd64ac105d9e348e2bfd2c209bbdb6
Michael Dally <mddally@gmail.com>
2022-03-15 13:42:47 -0400

    commit tree 81f8fce4627872b031baa0697811e5999c9f51f4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.filter{|list_word| list_word.split("").sort == @word.split("").sort}
+    end
+        
+end
